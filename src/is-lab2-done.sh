4c99929c410439a0cab46819957ebf96c9c45cc2
Joey Lawrance <lawrancej@wit.edu>
2015-05-21 12:37:23 -0400

    Stupid me.

--- src/is-lab2-done.sh
+++ src/is-lab2-done.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 expected="$(cat helloworld.bf)"
-actual="$(brainfuck.exe helloworld.bf)"
+actual="$(./brainfuck.exe helloworld.bf)"
 
 if [ "$expected" == "$actual" ]; then
     echo "Hooray, you're done!"
